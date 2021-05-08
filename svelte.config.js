4db56c4104af91df4af94596554e2e02391b2576
vhollo <vhollo@users.noreply.github.com>
2021-05-08 19:36:17 +0200

    mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
+import { mdsvex } from "mdsvex";
+import { mdsvexConfig } from "./mdsvex.config.js";
 /** @type {import('@sveltejs/kit').Config} */
 //import adapter from '@sveltejs/adapter-netlify'
 import adapter from '@sveltejs/adapter-static'
 const config = {
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+	preprocess: [
+		mdsvex(mdsvexConfig),
+	],
 	kit: {
 		adapter: adapter(),
 		target: '#svelte'
